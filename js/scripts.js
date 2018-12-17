8857ab2655171bad55ee7710e1b7816c39e289ca
Stella-Ndirangu <stellaw.ndirangu@gmail.com>
2018-12-17 19:54:36 +0300

    Add function to disable and enable player divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("#intro-form form").submit(function(event){
+    $("#p2").addClass("disableDiv");
     var players = ["player1","player2"]
 
     players.forEach(function(playername) {
@@ -7,8 +8,34 @@ $(document).ready(function(){
       $("." + playername).text(userInput);
     });
 
-    $("#intro-form").hide();
+    //$("#intro-form").hide();
     $("#game").show();
+
     event.preventDefault();
+  });
+});
+
+var side1 = new Image();
+side1.src = "images/1.png";
+var side2 = new Image();
+side2.src = "images/2.png";
+var side3 = new Image();
+side3.src = "images/3.png";
+var side4 = new Image();
+side4.src = "images/4.png";
+var side5 =  new Image();
+side5.src = "images/5.png";
+var side6 = new Image();
+side6.src = "images/6.png";
+
+function rollDice(){
+  var randomRoll = Math.floor(Math.random()*7)
+  document.images["die"].src=eval("side"+randomRoll+".src");
+};
+
+$(document).ready(function(){
+  $("#game #passbutton1").click(function(event){
+    $("#p1").addClass("disableDiv");
+    $("#p2").removeClass("disableDiv");
   });
 });
